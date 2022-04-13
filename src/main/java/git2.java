fa5e2cae09630e9b6a015aa1a87dccbfec80292f
Peter <nsh157277@163.com>
2022-04-13 18:30:18 +0800

    自我第二次提交

--- src/main/java/git2.java
+++ src/main/java/git2.java
@@ -2,5 +2,9 @@ public class git2 {
 
     public static void main(String[] args) {
         System.out.println("hello this 2");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("你好这是第二");
+        }
     }
 }
